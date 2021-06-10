17a510c8b5c6c5aec8f9feb68df9adaaeb3a351c
idaehui <idaehui@192.168.0.38>
2021-06-10 17:36:36 +0900

    devB branch

--- MyGIT/src/A.java
+++ MyGIT/src/A.java
@@ -1,4 +1,11 @@
 
 public class A {
-
+	private static int x = 3;
+	private static int y = 2;
+	
+	public static void main(String[] args) {
+		B plus = new B();
+		
+		System.out.println(plus.cal(x, y));
+	}
 }
